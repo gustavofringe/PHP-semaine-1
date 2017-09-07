0d67072c3fc26d74f6e3015e47f499fc9268e09c
Dussart Guillaume <dussartguillaume@orange.fr>
2017-09-07 09:42:14 +0200

    add database

---
+++ db/db.php
@@ -0,0 +1,33 @@
+<?php
+$db = [
+    1 => [
+        'image' => '../img/car1.jpg',
+        'content' => 'Lorem1 ipsum dolor sit amet, consectetur adipisicing elit. Asperiores consequuntur debitis earum facere harum libero maxime, nulla perferendis qui vitae! Animi cumque impedit minima natus nostrum officiis repellat sed voluptates.',
+        'autor' => 'Guillaume'
+    ],
+    2 => [
+        'image' => '../img/car2.jpg',
+        'content' => 'Lorem2 ipsum dolor sit amet, consectetur adipisicing elit. Asperiores consequuntur debitis earum facere harum libero maxime, nulla perferendis qui vitae! Animi cumque impedit minima natus nostrum officiis repellat sed voluptates.',
+        'autor' => 'Vincent'
+    ],
+    3 => [
+        'image' => '../img/car3.jpg',
+        'content' => 'Lorem3 ipsum dolor sit amet, consectetur adipisicing elit. Asperiores consequuntur debitis earum facere harum libero maxime, nulla perferendis qui vitae! Animi cumque impedit minima natus nostrum officiis repellat sed voluptates.',
+        'autor' => 'Guillaume'
+    ],
+    4 => [
+        'image' => '../img/car4.jpg',
+        'content' => 'Lorem4 ipsum dolor sit amet, consectetur adipisicing elit. Asperiores consequuntur debitis earum facere harum libero maxime, nulla perferendis qui vitae! Animi cumque impedit minima natus nostrum officiis repellat sed voluptates.',
+        'autor' => 'Vincent'
+    ],
+    5 => [
+        'image' => '../img/car5.jpg',
+        'content' => 'Lorem5 ipsum dolor sit amet, consectetur adipisicing elit. Asperiores consequuntur debitis earum facere harum libero maxime, nulla perferendis qui vitae! Animi cumque impedit minima natus nostrum officiis repellat sed voluptates.',
+        'autor' => 'Guillaume'
+    ],
+    6 => [
+        'image' => '../img/car6.jpg',
+        'content' => 'Lorem6 ipsum dolor sit amet, consectetur adipisicing elit. Asperiores consequuntur debitis earum facere harum libero maxime, nulla perferendis qui vitae! Animi cumque impedit minima natus nostrum officiis repellat sed voluptates.',
+        'autor' => 'Vincent'
+    ],
+];
