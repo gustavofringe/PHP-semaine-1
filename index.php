787755b2cdce0763abd42e39a7cc3f33ed4618de
Dussart Guillaume <dussartguillaume@orange.fr>
2017-09-08 08:43:14 +0200

    define base url

--- index.php
+++ index.php
@@ -1,29 +1,4 @@
-<!doctype html>
-<html class="no-js" lang="">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="apple-touch-icon" href="apple-touch-icon.png">
-        <!-- Favicon -->
-         <link rel="icon" type="image/png" href="img/favicon.jpg" />
-
-        <!-- CSS Files -->
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/main.css">
-        <!-- End CSS Files -->
-
-        <!-- font files -->
-        <link href="https://" rel="stylesheet">
-        <!-- End font files -->
-
-
-        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
-    </head>
-    <body>
         <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
---
+++ partials/header.php
@@ -0,0 +1,29 @@
+<?php
+define ('BASE_URL', dirname(dirname($_SERVER['SCRIPT_NAME'])));
+?>
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="<?= BASE_URL; ?>img/favicon.jpg" />
+
+    <!-- CSS Files -->
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <!-- End CSS Files -->
+
+    <!-- font files -->
+    <link href="https://" rel="stylesheet">
+    <!-- End font files -->
+
+
+    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+<body>
